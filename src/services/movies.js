ec26dcec30e8f6a25738e81e85e81e3aa7bec703
Mario <sks_894@hotmail.com>
2023-04-24 20:26:11 -0400

    Add api key to environment variables

--- src/services/movies.js
+++ src/services/movies.js
@@ -1,10 +1,10 @@
-const API_KEY = 'd6fc0a07';
-
 export async function searchMovies({ search }) {
   if (search === '') return null;
 
   try {
-    const response = await fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${search}`);
+    const response = await fetch(
+      `https://www.omdbapi.com/?apikey=${import.meta.env.VITE_OMDb_API}&s=${search}`
+    );
     const json = await response.json();
 
     const movies = json.Search;
